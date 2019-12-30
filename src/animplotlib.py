8666b13d2dfc0401cab91100670e7c0b1c2e7aa4
Aymen Hafeez <aymen@aymens-mbp.home>
2019-12-30 22:55:29 +0000

    added shape_data function and merged AnimPlot and AnimSimple classes

--- src/animplotlib.py
+++ src/animplotlib.py
@@ -1,71 +1,70 @@
 from matplotlib.animation import FuncAnimation
-
-# TODO: Merge AnimSimple and AnimPlot classes
-
-
-class AnimSimple:
-    """
-    Produces an animated 2-D plot. Plots each point rather than all points
-    up until the ith point.
-    """
-
-    def __init__(self, fig, lines, x, y, **kwargs):
-
-        def _init():
-            lines.set_data([], [])
-            return lines,
-
-        def _animate(i):
-            lines.set_data(x[i], y[i])
-            return lines,
-
-        self.anim = FuncAnimation(fig, _animate, init_func=_init, blit=True,
-                                  **kwargs)
+import numpy as np
 
 
 class AnimPlot:
-    '''Produces an animated 2-D plot.'''
+    """
+    Produces an animated 2-D plot.
 
-    def __init__(self, fig, lines, x, y, plot_speed=10, **kwargs):
+    Parameters
+    ==========
+    fig        : matplotlib figure
+    ax         : matplotlib axes
+    lines      : empty matplotlib plot
+    x, y       : points being plotted
+    trail      : if True, all points up to the ith point are plotted each frame
+                 if False, only the ith point is plotted each frame
+    plot_speed : int
+    **kwargs   : other arguments passable into
+                 matplotlib.animation.FuncAnimation()
+    """
+    def __init__(self, fig, lines, x, y, trail=True, plot_speed=10, **kwargs):
 
         def _init():
             lines.set_data([], [])
             return lines,
+
+        if len(x) != len(y):
+            raise ValueError('x and y must be the same size.')
 
         def _animate(i):
             lines.set_data(x[:plot_speed * i], y[:plot_speed * i])
             return lines,
 
-        self.anim = FuncAnimation(fig, _animate, init_func=_init, interval=1, blit=True,
-                                  **kwargs)
+        def _animate_single_point(i):
+            lines.set_data(x[plot_speed * i], y[plot_speed * i])
+            return lines,
+
+        if trail == True:
+            animation = FuncAnimation(fig, _animate, init_func=_init,
+                                           interval=1, blit=True, **kwargs)
+        else:
+            animation = FuncAnimation(fig, _animate_single_point,
+                                           init_func=_init, blit=True,
+                                           **kwargs)
 
 
 class AnimPlot3D:
-
-    # TODO: Add array reshape functionality to class
-    
     """
-    Produces an animated 3-D plot. Array shape must be (1, n, 3), where
-    n is the number of data points.
+    Produces an animated 3-D plot.
 
-    Parameters:
-    ===========
-    fig = matplotlib.pyplot.figure()
-    ax = axes
-    lines = [ax.plot([], [], [])]
-    points = [ax.plot([], [], [], 'o')]
-    array = (1, n, 3) array
-    plot_speed = Natural number 
-    rotation_speed = Natural number, proportional to plot_speed
-        ---> keep blit=False for this
-    l_num = Number of points plotted to lines each frame
-    p_num = Number of points plotted to points each frame
-    **kwargs = Other arguments passable into matplotlib.animation.FuncAnimation()
-
-    See examples to better understand how to use each parameter.
+    Parameters
+    ==========
+    fig            : matplotlib figure
+    ax             : matplotlib axes
+    lines          : empty matplotlib plot contained in a list
+    points         : empty matplotlib plot contained in a list; shows the
+                     ith point being plotted
+    x, y, z        : list; points being plotted
+    plot_speed     : int 
+    rotation_speed : int; proportional to plot_speed
+                     ---> keep blit=False (default) for this
+    l_num          : number of points plotted to 'lines' each frame
+    p_num          : number of points plotted to 'points' each frame
+    **kwargs       : other arguments passable into
+                     matplotlib.animation.FuncAnimation()
     """
-
-    def __init__(self, fig, ax, lines, points, array, plot_speed=10,
+    def __init__(self, fig, ax, lines, points, x, y, z, plot_speed=10,
                  rotation_speed=0.0, l_num=0, p_num=-1, **kwargs):
 
         def _init():
@@ -77,7 +76,14 @@ class AnimPlot3D:
                 self.point.set_3d_properties([])
             return lines + points
 
+        def shape_data(x, y, z):
+            data = np.array([x, y, z])
+            data_transpose = np.array([data]).T
+            data_plot = np.reshape(data_transpose, (1, len(x), 3), order='C')
+            return data_plot
+
         def _animate(i):
+            array = shape_data(x, y, z)
             i = (plot_speed * i) % array.shape[1]
 
             for self.line, self.point, self.data in zip(lines, points, array):
@@ -90,7 +96,6 @@ class AnimPlot3D:
 
             ax.view_init(30, rotation_speed * i)
             fig.canvas.draw()
-
             return lines + points
 
         self.anim = FuncAnimation(fig, _animate, init_func=_init, interval=1,
