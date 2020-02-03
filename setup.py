71fea4f9229a54cf7449e8d1cc8b5af1dd3378a2
aymen <aymennh@gmail.com>
2020-02-03 01:54:04 +0000

    v0.1.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='animplotlib',
-    version='0.1.4',
+    version='0.1.5',
     author='Aymen Hafeez',
     author_email='aymennh@gmail.com',
     packages=['animplotlib',],
