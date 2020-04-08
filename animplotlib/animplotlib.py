8b9fb7b0e78f01c73fd497e2d1366a28ea395292
aymen <aymennh@gmail.com>
2020-04-08 13:55:18 +0100

    Updated docs to include save_as option

--- animplotlib/animplotlib.py
+++ animplotlib/animplotlib.py
@@ -14,6 +14,8 @@ class AnimPlot:
     trail      : if True, all points up to the ith point are plotted each frame
                  if False, only the ith point is plotted each frame
     plot_speed : int
+    save_as    : str; file name to save animation as gif to current working
+                 directory
     **kwargs   : other arguments passable into
                  matplotlib.animation.FuncAnimation()
     """
@@ -35,7 +37,7 @@ class AnimPlot:
             lines.set_data(x[plot_speed * i], y[plot_speed * i])
             return lines,
 
-        if trail == True:
+        if trail is True:
             anim = FuncAnimation(fig, _animate, init_func=_init,
                                  interval=1, blit=True, **kwargs)
         else:
@@ -60,15 +62,17 @@ class AnimPlot3D:
     points         : empty matplotlib plot contained in a list; shows the
                      ith point being plotted
     x, y, z        : list; points being plotted
-    plot_speed     : int 
+    plot_speed     : int
     rotation_speed : int; proportional to plot_speed
                      ---> keep blit=False (default) for this
-    l_num          : number of points plotted to 'lines' each frame
-    p_num          : number of points plotted to 'points' each frame
+    l_num          : int; number of points plotted to 'lines' each frame
+    p_num          : int; number of points plotted to 'points' each frame
+    save_as        : str; file name to save animation as gif to current working
+                     directory
     **kwargs       : other arguments passable into
                      matplotlib.animation.FuncAnimation()
     """
-    def __init__(self, fig, ax, lines, points, x, y, z, plot_speed=10, 
+    def __init__(self, fig, ax, lines, points, x, y, z, plot_speed=10,
                  rotation_speed=0, l_num=0, p_num=1, save_as=None,
                  blit=True, **kwargs):
 
