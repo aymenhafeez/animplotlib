9698384cb3a2c0633d6f9be6aa0c13d44ed07441
Aymen Hafeez <aymen@aymens-mbp.home>
2023-09-06 02:32:38 +0100

    testing fix to 2D animation _animate function

--- animplotlib/animplotlib.py
+++ animplotlib/animplotlib.py
@@ -1,26 +1,64 @@
 from matplotlib.animation import FuncAnimation, PillowWriter
+import matplotlib.pyplot as plt
 import numpy as np
 
 
-class AnimPlot:
-    """
-    Produces an animated 2-D plot.
+# class AnimPlot:
+#     """
+#     Produces an animated 2-D plot.
+#
+#     Parameters
+#     ==========
+#     fig        : matplotlib figure
+#     lines      : empty matplotlib plot
+#     x, y       : list; points being plotted
+#     trail      : if True, all points up to the ith point are plotted each frame
+#                  if False, only the ith point is plotted each frame
+#     plot_speed : int
+#     save_as    : str; file name to save animation as gif to current working
+#                  directory
+#     **kwargs   : other arguments passable into
+#                  matplotlib.animation.FuncAnimation()
+#     """
+#     def __init__(self, fig, lines, x, y, trail=True, plot_speed=10,
+#                  save_as=None, **kwargs):
+#         self.x = x
+#         self.y = y
+#         self.plot_speed = plot_speed
+#         self.lines = lines
+#
+#         def _init():
+#             self.lines.set_data([], [])
+#             return self.lines,
+#
+#         if len(self.x) != len(self.y):
+#             raise ValueError('x and y must be the same size.')
+#
+#         def _animate(i):
+#             start_idx = 0
+#             end_idx = min(self.plot_speed * (i + 1), len(self.x))
+#             self.lines.set_data(self.x[:end_idx], self.y[:end_idx])
+#             return self.lines,
+#
+#         def _animate_single_point(i):
+#             self.lines.set_data(self.x[plot_speed * i], self.y[plot_speed * i])
+#             return self.lines,
+#
+#         if trail is True:
+#             anim = FuncAnimation(fig, _animate, init_func=_init, frames=len(x)//plot_speed,
+#                                  interval=1, blit=True, **kwargs)
+#         else:
+#             anim = FuncAnimation(fig, _animate_single_point,
+#                                  init_func=_init, frames=len(x)//plot_speed, blit=True, **kwargs)
+#
+#         if save_as is not None:
+#             anim.save(save_as + '.gif', writer=PillowWriter(fps=60))
+#         else:
+#             pass
+#
 
-    Parameters
-    ==========
-    fig        : matplotlib figure
-    lines      : empty matplotlib plot
-    x, y       : list; points being plotted
-    trail      : if True, all points up to the ith point are plotted each frame
-                 if False, only the ith point is plotted each frame
-    plot_speed : int
-    save_as    : str; file name to save animation as gif to current working
-                 directory
-    **kwargs   : other arguments passable into
-                 matplotlib.animation.FuncAnimation()
-    """
-    def __init__(self, fig, lines, x, y, trail=True, plot_speed=10,
-                 save_as=None, **kwargs):
+class AnimPlot:
+    def __init__(self, fig, lines, x, y, plot_speed=10, save_as=None):
         self.x = x
         self.y = y
         self.plot_speed = plot_speed
@@ -30,30 +68,19 @@ class AnimPlot:
             self.lines.set_data([], [])
             return self.lines,
 
-        if len(self.x) != len(self.y):
-            raise ValueError('x and y must be the same size.')
-
         def _animate(i):
             start_idx = 0
             end_idx = min(self.plot_speed * (i + 1), len(self.x))
             self.lines.set_data(self.x[:end_idx], self.y[:end_idx])
             return self.lines,
 
-        def _animate_single_point(i):
-            self.lines.set_data(self.x[plot_speed * i], self.y[plot_speed * i])
-            return self.lines,
-
-        if trail is True:
-            anim = FuncAnimation(fig, _animate, init_func=_init, frames=len(x)//plot_speed,
-                                 interval=1, blit=True, **kwargs)
-        else:
-            anim = FuncAnimation(fig, _animate_single_point,
-                                 init_func=_init, frames=len(x)//plot_speed, blit=True, **kwargs)
+        anim = FuncAnimation(fig, _animate, init_func=_init, frames=len(x)//plot_speed,
+                             interval=1, blit=True)
 
         if save_as is not None:
             anim.save(save_as + '.gif', writer=PillowWriter(fps=60))
         else:
-            pass
+            plt.show()
 
 
 class AnimPlot3D:
