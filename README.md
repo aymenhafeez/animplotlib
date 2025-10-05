0c086894027e12020c7bd2e8d63e6e5eb5ee1088
Aymen Hafeez <hafeezaymen2357@gmail.com>
2025-10-05 22:07:39 +0100

    major rewrite, README updated with new user instructions

--- README.md
+++ README.md
@@ -25,7 +25,8 @@ basic plot of an Euler spiral. An Euler spiral can be obtained by plotting
 the [Fresnel integrals](https://en.wikipedia.org/wiki/Fresnel_integral),
 which can be generated using `scipy.special`.
 
-Import the necessary libraries and create a `matplotlib` figure and axes:
+Import the necessary libraries and generate the points being plotted, in this
+case the fresnel integrals:
 
 ```python
 import animplotlib as anim
@@ -33,41 +34,37 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.special as sc
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
-```
-
-Generate the points being plotted:
-
-```python
-x = np.linspace(-10, 10, 2500)
+x = np.linspace(-7, 7, 5000)
 y, z = sc.fresnel(x)
+
 ```
 
-Create two empty `matplotlib` plots: one to plot the points up to the current
-most point (i.e. the 'line') and one to plot the current most point:
+Create a figure and axis and then add two empty `matplotlib` plots: one to plot the points
+up to the current most point (i.e. the 'line') and one to plot the current most
+point:
 
 ```python
-line, = ax.plot([], [], lw=1)
+fig, ax = plt.subplots()
+line, = ax.plot([], [], lw=2)
 point, = ax.plot([], [], 'o')
 
-ax.set_xlim(-1, 1)
-ax.set_ylim(-1, 1)
+ax.set_xlim(np.min(y), np.max(y))
+ax.set_ylim(np.min(z), np.max(z))
+ax.set_title("Animated 2D Fresnel Plot")
 ```
  
-Call the `AnimPlot` class and show the plot:
+Call the `AnimPlot` class:
 
 ```python
-animation = anim.AnimPlot(fig, line, point, y, z, l_num=len(x),
-                          plot_speed=5)
-plt.show()
+anim.AnimPlot(fig, line, point, y, z, plot_speed=5, l_num=len(x))
 ```
 
 `l_num` is the number of points before the current most point being plotted to
 `line`. The default value is set to 10, however in this example it makes sense
-to set it to the same length as `x` (i.e. all the points before the current most
-point are plotted). Similarly, an argument `p_num` can be passed to determine
-the number of points being plotted to `point`. This is set to 1 by default.
+to set it to the same length as `x` (i.e. all the points before the current
+most point are plotted). Think of this as a trail. Similarly, an argument
+`p_num` can be passed to determine the number of points being plotted to
+`point`. This is set to 1 by default.
 
 Optional arguments:
 * `plot_speed` (`int`) : set to 10 by default.
@@ -91,40 +88,40 @@ frame (the orange point in the gif).
 
 ### AnimPlot3D
 
-Creating a 3-D animated plot is similar to creating a 2-D plot but with a
-few additional steps.
+Creating a 3-D animated plot is very similar to creating a 2-D animated plot.
+Continuing with the Euler spiral example, the first step again is to import the
+necessary libraries and generate the points being plotted:
 
 ```python
 import animplotlib as anim
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 import scipy.special as sc
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-
-x = np.linspace(-10, 10, 3000)
+x = np.linspace(-7, 7, 5000)
 y, z = sc.fresnel(x)
 ```
 
-For 3-D plots, two empty `matplotlib` plots must be created:
+Next, create a figure and axis with 3-D projection and then add two empty
+plots, this time with three list inputs:
 
 ```python
-lines, = [ax.plot([], [], [])]
-points, = [ax.plot([], [], [], 'o')]
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+lines, = ax.plot([], [], [], lw=1)
+points, = ax.plot([], [], [], 'ro', markersize=5)
 ```
 
-The second plot, `points`, by default plots the 'ith' point each frame. After
-that set the x, y and z limits and call the `AnimPlot3D` class.
+After that set the x, y and z limits and call the `AnimPlot3D` class.
 
 ```python
-ax.set_xlim(-10, 10)
-ax.set_ylim(-1, 1)
-ax.set_zlim(-1, 1)
+ax.set_xlim(np.min(x), np.max(x))
+ax.set_ylim(np.min(y), np.max(y))
+ax.set_zlim(np.min(z), np.max(z))
+ax.set_title("Animated 3D Fresnel Plot")
 
-animation = anim.AnimPlot3D(fig, ax, lines, points, x, y, z, plot_speed=5)
-plt.show()
+anim.AnimPlot3D(fig, ax, [lines], [points], x, y, z, plot_speed=5)
 ```
 
 <!-- ![](examples/gifs/fresnel_3d.gif =450x402) -->
@@ -137,13 +134,12 @@ plt.show()
 
 Optional arguments:
 * `plot_speed` (`int`) : set to 10 by default.
-* `rotation_speed` (`int`) : proportional to `plot_speed`. Off by default,
-enabled by setting a value.
+* `rotation_speed` (`int`) : Off by default, enabled by setting a value.
 * `l_num` (`int`) : The number of points being plotted to `lines` each frame. By
-default, all the points up until the current point get plotted.
+default, all the points up until the current point get plotted (`l_num=0`.
 * `p_num` (`int`) : The number of points being plotted to `points` each frame. By
 default, this is set to 1, i.e. only the current most point is plotted each
-frame (the orange point in the gif).
+frame (the red point in the gif).
 * `save_as` (`str`) : file name to save the animation as a gif in the
   current working directory.
 * `**kwargs` : other arguments passable into
--- animplotlib/animplotlib.py
+++ animplotlib/animplotlib.py
@@ -1,133 +1,100 @@
 from matplotlib.animation import FuncAnimation, PillowWriter
 import matplotlib.pyplot as plt
 import numpy as np
-
+import math
 
 class AnimPlot:
     """
     Produces an animated 2-D plot.
-
-    Parameters
-    ==========
-    fig        : matplotlib figure
-    line      : empty matplotlib plot to plot all points up to the current point
-    point     : empty matplotlib plot to plot the current most point
-    x, y       : list; points being plotted
-    plot_speed : int
-    l_num          : int; number of points plotted to 'line' each frame
-    p_num          : int; number of points plotted to 'point' each frame
-    save_as    : str; file name to save animation as gif to current working
-                 directory
-    **kwargs   : other arguments passable into
-                 matplotlib.animation.FuncAnimation()
     """
     def __init__(self, fig, line, point, x, y, plot_speed=10, l_num=1,
-                  p_num=1, save_as=None, **kwargs):
-        self.x = x
-        self.y = y
+                 p_num=1, save_as=None, **kwargs):
+        self.x = np.asarray(x)
+        self.y = np.asarray(y)
         self.plot_speed = plot_speed
         self.line = line
         self.point = point
         self.l_num = l_num
         self.p_num = p_num
+        self.save_as = save_as
+        self.kwargs = kwargs
 
-        def _init():
-            trailing_x, trailing_y  = np.array([]), np.array([])
-            current_x, current_y  = np.array([]), np.array([])
-
-            self.line.set_data(trailing_x, trailing_y)
-            self.point.set_data(current_x, current_y)
-
-            return self.line, self.point
-
-        def _animate(i):
-            end_idx = min(self.plot_speed * (i + 1), len(self.x))
-            trailing_x = self.x[max(0, end_idx - self.l_num):end_idx]
-            trailing_y = self.y[max(0, end_idx - self.l_num):end_idx]
-            current_x = self.x[end_idx - self.p_num:end_idx]
-            current_y = self.y[end_idx - self.p_num:end_idx]
-
-            self.line.set_data(trailing_x, trailing_y)
-            self.point.set_data(current_x, current_y)
-
-            return self.line, self.point
-
-        anim = FuncAnimation(fig, _animate, init_func=_init, frames=len(x)//plot_speed,
-                             interval=1, blit=True, **kwargs)
+        self.anim = FuncAnimation(
+            fig, self._animate, init_func=self._init,
+            frames=math.ceil(len(self.x) / self.plot_speed),
+            interval=1, blit=True, **kwargs
+        )
 
         if save_as is not None:
-            anim.save(save_as + '.gif', writer=PillowWriter(fps=60))
+            self.anim.save(save_as + '.gif', writer=PillowWriter(fps=60))
         else:
             plt.show()
+
+    def _init(self):
+        self.line.set_data([], [])
+        self.point.set_data([], [])
+        return self.line, self.point
+
+    def _animate(self, i):
+        end_idx = min(self.plot_speed * (i + 1), len(self.x))
+        trailing_x = self.x[max(0, end_idx - self.l_num):end_idx]
+        trailing_y = self.y[max(0, end_idx - self.l_num):end_idx]
+        current_x = self.x[end_idx - self.p_num:end_idx]
+        current_y = self.y[end_idx - self.p_num:end_idx]
+        self.line.set_data(trailing_x, trailing_y)
+        self.point.set_data(current_x, current_y)
+        return self.line, self.point
 
 
 class AnimPlot3D:
     """
     Produces an animated 3-D plot.
-
-    Parameters
-    ==========
-    fig            : matplotlib figure
-    ax             : matplotlib axes
-    lines          : empty matplotlib plot contained in a list
-    points         : empty matplotlib plot contained in a list; shows the
-                     ith point being plotted
-    x, y, z        : list; points being plotted
-    plot_speed     : int
-    rotation_speed : int; proportional to plot_speed
-                     ---> keep blit=False (default) for this
-    l_num          : int; number of points plotted to 'lines' each frame
-    p_num          : int; number of points plotted to 'points' each frame
-    save_as        : str; file name to save animation as gif to current working
-                     directory
-    **kwargs       : other arguments passable into
-                     matplotlib.animation.FuncAnimation()
     """
     def __init__(self, fig, ax, lines, points, x, y, z, plot_speed=10,
                  rotation_speed=0, l_num=0, p_num=1, save_as=None,
                  blit=True, **kwargs):
-
         self.fig = fig
         self.ax = ax
         self.lines = lines
         self.points = points
-        self.x = x
-        self.y = y
-        self.z = z
+        self.x = np.asarray(x)
+        self.y = np.asarray(y)
+        self.z = np.asarray(z)
         self.plot_speed = plot_speed
         self.rotation_speed = rotation_speed
         self.l_num = l_num
         self.p_num = p_num
+        self.save_as = save_as
+        self.kwargs = kwargs
 
-        def _init():
-            empty_array = np.array([], dtype=float)
-            for line, point in zip(self.lines, self.points):
-                line.set_data(empty_array, empty_array)
-                line.set_3d_properties(empty_array)
-
-                point.set_data(empty_array, empty_array)
-                point.set_3d_properties(empty_array)
-            return self.lines + self.points
-
-        def _animate(i):
-            i = (self.plot_speed * i) % len(self.x)
-
-            for line, point in zip(self.lines, self.points):
-                x_vals, y_vals, z_vals = self.x[:i], self.y[:i], self.z[:i]
-                line.set_data(x_vals[-self.l_num:], y_vals[-self.l_num:])
-                line.set_3d_properties(z_vals[-self.l_num:])
-
-                point.set_data(x_vals[-self.p_num:], y_vals[-self.p_num:])
-                point.set_3d_properties(z_vals[-self.p_num:])
-
-            if self.rotation_speed != 0:
-                self.ax.view_init(30, self.rotation_speed * i)
-                self.fig.canvas.draw()
-
-            return self.lines + self.points
-
-        self.anim = FuncAnimation(self.fig, _animate, init_func=_init, interval=1,
-                                 blit=blit, **kwargs)
+        self.anim = FuncAnimation(
+            self.fig, self._animate, init_func=self._init,
+            frames=len(self.x) // self.plot_speed,
+            interval=1, blit=blit, **kwargs
+        )
 
         if save_as is not None:
             self.anim.save(save_as + '.gif', writer=PillowWriter(fps=60))
+        else:
+            plt.show()
+
+    def _init(self):
+        for line, point in zip(self.lines, self.points):
+            line.set_data([], [])
+            line.set_3d_properties([])
+            point.set_data([], [])
+            point.set_3d_properties([])
+        return self.lines + self.points
+
+    def _animate(self, i):
+        idx = (self.plot_speed * i) % len(self.x)
+        for line, point in zip(self.lines, self.points):
+            x_vals, y_vals, z_vals = self.x[:idx], self.y[:idx], self.z[:idx]
+            line.set_data(x_vals[-self.l_num:], y_vals[-self.l_num:])
+            line.set_3d_properties(z_vals[-self.l_num:])
+            point.set_data(x_vals[-self.p_num:], y_vals[-self.p_num:])
+            point.set_3d_properties(z_vals[-self.p_num:])
+        if self.rotation_speed != 0:
+            self.ax.view_init(30, self.rotation_speed * idx)
+            self.fig.canvas.draw()
+        return self.lines + self.points
